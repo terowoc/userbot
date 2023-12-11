648e9a6ed7cd95cd424d969d10b341c150f25a08
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 <terowoc@mail.ru>
2023-12-11 14:52:31 +0500

    fix

---
+++ config.example.py
@@ -0,0 +1,13 @@
+account = dict(
+    api_id = 11111,
+    api_hash = '23452345',
+    session_string = 'akshdfkashdfk',
+)
+
+bio = [
+    'Back End Developer.',
+    'azimboev.uz',
+    'Бэкэнд разработчик из Ташкента.',
+    'Жаст ду ит!',
+    'PHP & Laravel - ♾❤️'
+]
