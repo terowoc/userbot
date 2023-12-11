1ab40c83d82029d08cce9f6a2906727bba75d84f
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 <terowoc@mail.ru>
2023-12-11 14:45:08 +0500

    init

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 account = dict(
-    api_id = 28967306,
-    api_hash = 'd345794982b1dafa4471445b7072e7af',
-    session_string = 'AgAhMYcd5aQYa_HbWTpatYm5f6qs5xkIuO1Lu7eDPRu0-It0CWHMggugcMplULN6_bKUrfub4CelEPW67GFphc0xiFmuMuRo7CnqTBm2R4DO4j6L0PufPQJuJw4EEjHNotq-Bv0WcqFWm9KG2lcRdJEch8j1wDJuSZUfLuKilRqyySPaRyIa5bsx5JpqJuJ3sdz4GMFXH57epRI3HmvttPzZuY3DJjIOusHMZ7t2IZnpKkpY5Iaok5v8KB_Ef8TLBsTl7HYAEEPtiaAG7zxfCWXW4lMDNVLchT01_-Y6d57od5ruh38FyVG5gRaznlslLb92dSVDSgRvHpHIkO2z0QfGAAAAAZrmK_sA',
+    api_id = 24037862,
+    api_hash = 'e0c5a0b6adc8abed6f873066d14ead94',
+    session_string = 'AgFuyeYARetAA0xDu9RCe3YeLA7f-aa823SR6uahhzD14wHTRqKNpYzMltTU928S15xhRXnZh6Gi9dF6jb8BSYYniC635R6n8F3z0WAViAPeBY6YtrnzduJOcpV1cZJ37kRnK54YYWULACa6mwA7aXamfCL5-6jgKOn1qpQntMmU6dNtaHKC7Vlb70YV1HEkFaPJDNpCdGoPoHgoMoP5uW8pmhFD_HHZtRK21Cvo3W1vquySty607Rwr9MsGd9w10o1z1DufrSxCiL6pEXEyCn2Kj9lXn3TQv2QEk180CyAtP5F0QwxsBsUd_qWeZWrn-OxfaIHrik9IqMyOEyN0PBK0eOj8igAAAAGFhpkWAA',
 )
+
+bio = [
+    'Back End Developer.',
+    'azimboev.uz',
+    'Бэкэнд разработчик из Ташкента. azimboev.uz',
+]
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import config
 from pyrogram import Client
+import random
 
 async def main():
         try:
             async with Client("account", api_id=config.account['api_id'], api_hash=config.account['api_hash'], session_string=config.account['session_string']) as app:
-            	
+            	await app.update_profile(bio=random.choice(config.bio))
+
         except Exception as e:
         	print(e)
                 
