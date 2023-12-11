99f6969531c74994fabcc391d1051d5a63b91ebf
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 <terowoc@mail.ru>
2023-12-11 14:45:28 +0500

    init

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ async def main():
         try:
             async with Client("account", api_id=config.account['api_id'], api_hash=config.account['api_hash'], session_string=config.account['session_string']) as app:
             	await app.update_profile(bio=random.choice(config.bio))
-
+            	print('Done !')
+            	
         except Exception as e:
         	print(e)
                 
