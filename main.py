fc226b77d277858668e56030b6144a648008c2e0
Abdulloh <terowoc@mail.ru>
2024-04-17 19:31:40 +0500

    modified:   main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import config
 import random
 import asyncio
-from pyrogram import Client, filters
+from pyrogram import Client
 
-app = Client("account", api_id=config.account['api_id'], api_hash=config.account['api_hash'], session_string=config.account['session_string'])
+async def main():
+  async with Client("account", api_id=config.account['api_id'], api_hash=config.account['api_hash'], session_string=config.account['session_string']) as app:
+    while True:
+      bio = random.choice(config.bio)
+      await app.update_profile(bio=bio)
+      await asyncio.sleep(1200)
 
-@app.on_message(filters.text & filters.private)
-async def echo(client, message):
-  if message.chat.type == 'private':
-    if "salam" in message.text:
-      await message.reply("Salam aleykum " + message.from_user.mention)
-      
-
-app.run()
+asyncio.run(main())
